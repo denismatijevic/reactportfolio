4f592a6a9f0d2771e30a4d5e57589fac39244281
denis matijevic <denisone90@gmail.com>
2020-10-31 20:43:58 -0600

    added about me

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -95,11 +95,23 @@ const Picture = () => {
                 </MDBView>
                 <div className="d-flex justify-content-between">
                 </div>
-                <p className="dark-grey-text">
-                  Sed ut perspiciatis unde voluptatem omnis iste natus error sit
-                  voluptatem accusantium doloremque laudantium, totam rem
-                  aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-                  architecto beatae.
+                <p className="dark-grey-text" id="about-me">
+                Hi! My name is Denis and I am a junior full stack web developer. I grew up in Croatia and I moved to the United
+                States in 2001. I currently live in Sandy, UT.
+                <br />
+                <br />
+                My background is in finance and transportation, I am a stock broker. I hold the series 7, SIE, and the series 63
+                licenses. I also hold an insurance license.
+                I've always been fascinated with web design and the nitty griddy of it, so I figured that I should learn it and
+                see what it looks like form a developers view point. I fell in love with it.
+                <br />
+                <br />
+                I'm a country boy at heart and I love going out into nature and exploring the beautiful mountains Utah has. I am
+                an avid hiker. I have a dog named Charlie, he is a GSP. He comes on a lot of adventures with me.
+                <br />
+                <br />
+                Currently I am attending school at The University of Utah, I'm enrolled in their coding bootcamp and I will be
+                graduating in Fall of 2020.
                 </p>
               </div>
               <div style={{
